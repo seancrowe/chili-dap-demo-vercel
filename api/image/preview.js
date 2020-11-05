8f2199caa3c065fc8a8179108eb85eea729d3327
seancrowe <retrounlocked@gmail.com>
2020-11-04 20:13:48 -0600

    Add preview endpoint and switched out properties images

---
+++ api/image/preview.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const {join} = require('path');
+
+module.exports = (req, res) => {
+    const {
+        query: { path, name },
+    } = req
+
+    try {
+        const image = fs.readFileSync(join(__dirname, 'images', `/${path}/${name}-Preview.jpg`));
+
+        res.setHeader("Content-Type", "image/jpeg");
+        res.send(image);
+    }
+    catch (e) {
+        const image = fs.readFileSync(join(__dirname, 'images', `/404.jpg`));
+
+        res.setHeader("Content-Type", "image/jpeg");
+        res.send(image);
+    }
+}
